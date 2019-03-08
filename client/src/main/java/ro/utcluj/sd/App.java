3438795dacf0b7879046453e5c2f54c0f752b659
Vlad Buzea <vlad.buzea@itiviti.com>
2019-03-08 11:16:43 +0200

    Folder Structure
    Change-Id: I4a34cfc1442dc69f69eb1aa1caba626057e589e2

---
+++ client/src/main/java/ro/utcluj/sd/App.java
@@ -0,0 +1,23 @@
+/*************************************************************************
+ * ULLINK CONFIDENTIAL INFORMATION
+ * _______________________________
+ *
+ * All Rights Reserved.
+ *
+ * NOTICE: This file and its content are the property of Ullink. The
+ * information included has been classified as Confidential and may
+ * not be copied, modified, distributed, or otherwise disseminated, in
+ * whole or part, without the express written permission of Ullink.
+ ************************************************************************/
+package ro.utcluj.sd;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application
+{
+    public void start(Stage primaryStage)
+    {
+
+    }
+}
